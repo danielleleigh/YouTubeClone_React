3c916601f7d447d51cc662779b0d43909b6e5572
Your Name <charles.luas.young@gmail.com>
2021-11-24 14:46:52 -0700

    work on search bar

---
+++ youtubeclone/src/components/videosearch/videosearch.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+
+// function SearchBar(props) {
+//     return (
+//         <div className="searchBar">
+//         <input type='text' name='search' value = {props.termToFilter} onChange={props.handleChange}/>
+//         <button type= 'submit'  onClick={()=>{props.filterSongs(" ")}}>search</button>
+//         </div>
+//     )
+// }
+
+// export default SearchBar;
+
+// const videoSearch = async => () => {
+//     let response = await axios.get("https://www.googleapis.com/youtube/v3/search?q=skiing&key=AIzaSyCTsMnnqzkaOvW4zYnh6rCJAUH2hXP0DbA");
+//     console.log(response.data)
+    
+// }
+
+// return (
+//     <div>
+//         <form  onSubmit={onFormSubmit}>
+//             <div className="field">
+//                 <label>Video Search</label>
+//                 <input type="text" placeholder="Search..." value={searchTerm} onChange={searchTermHandler} />
+//             </div>
+//         </form>
+//     </div>
+// )
+
+
+
+import React, { useState } from 'react'
+export const SearchBar = ({onSearchSubmit}) => {
+    const [searchTerm, setSearchTerm] = useState('')
+    const searchTermHandler = (evt) => {
+        evt.preventDefault()
+        setSearchTerm(evt.currentTarget.value)
+    }
+    const onFormSubmit = (evt) => {
+        evt.preventDefault()
+        onSearchSubmit(searchTerm)
+    }
+    return (
+        <div className="search-bar ui segment">
+            <form className="ui form" onSubmit={onFormSubmit}>
+                <div className="field">
+                    <label>Video Search</label>
+                    <input type="text" placeholder="Search..." value={searchTerm} onChange={searchTermHandler} />
+                </div>
+            </form>
+        </div>
+    );
