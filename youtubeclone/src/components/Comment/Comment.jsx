cd0b28d9a2ba8b482497efb9688ee35f11698f5e
Your Name <charles.luas.young@gmail.com>
2021-12-07 10:48:37 -0700

    Comment axios request

--- youtubeclone/src/components/Comment/Comment.jsx
+++ youtubeclone/src/components/Comment/Comment.jsx
@@ -1,18 +1,36 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 
-function Comment(props) {
-    const [comment,setComment] = useState();
+function Comment = (props) => {
+    const [comment,setComment] = useState('');
+    const [video, setVideo] = useState(props.currentVideoId);
 
     
 
     const handleChange = (event) => {
-       setComment({
-           [event.target.name]: event.target.value
-       }); 
+       setComment(event.target.value); 
     }
     
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        let newComment = {
+            comment:comment,
+            video_id:video,
+            like:0,
+            dislike:0,
+        }
+        await axios.post('http://127.0.0.1:8000/comment/', newComment)
+    }
 
-    const handleSubmit
+    return(
+        <div>
+            <form onSubmit={(e)=>handleSubmit(e)}>
+                <input onChange={handleChange} value = {comment}/>
+                <button type='submit'>Comment</button>
+            </form>
+        </div>
+    )
+
 }
 export default Comment;
